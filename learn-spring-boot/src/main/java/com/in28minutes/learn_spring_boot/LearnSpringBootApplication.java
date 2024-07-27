f9b88a76039c7937c5f67ffabb6a837763600105
cjm2681 <cjm2681@naver.com>
2024-07-27 20:24:57 +0900

    spring boot

--- learn-spring-boot/src/main/java/com/in28minutes/learn_spring_boot/LearnSpringBootApplication.java
+++ learn-spring-boot/src/main/java/com/in28minutes/learn_spring_boot/LearnSpringBootApplication.java
@@ -1,9 +1,19 @@
 package com.in28minutes.learn_spring_boot;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.AutoConfigurationExcludeFilter;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.TypeExcludeFilter;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.context.annotation.ComponentScan.Filter;
 
 @SpringBootApplication
+//@SpringBootConfiguration
+//@EnableAutoConfiguration
+//@ComponentScan
 public class LearnSpringBootApplication {
 
 	public static void main(String[] args) {
--- learn-spring-boot/src/main/java/com/in28minutes/learn_spring_boot/courses/controller/CourseController.java
+++ learn-spring-boot/src/main/java/com/in28minutes/learn_spring_boot/courses/controller/CourseController.java
@@ -14,8 +14,15 @@ public class CourseController {
 	// http://localhost:8080/courses
 	@GetMapping("/courses")
 	public List<Course> getAllCourses() {
-		return Arrays.asList(new Course(1, "Learn Microservices", "in28minutes"));
+		return Arrays.asList(new Course(1, "Learn Microservices", "in28minutes"),
+				new Course(2, "Learn Rull Stack with Angular and React", "in28minutes"));
 	}
 	
 	
+	//// http://localhost:8080/courses/1
+	@GetMapping("/courses/1")
+	public Course getCourseDetails() {
+		return new Course(1, "Learn Microservices 1", "in28minutes");
+				
+	}
 }
